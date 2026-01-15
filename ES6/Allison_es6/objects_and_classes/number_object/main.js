cf2f9fed5406308614b6940fce5f8fdf53d294f1
Ashley K Motsie <motsieashley31@gmail.com>
2026-01-15 22:44:35 +0200

    objects and classes commit

---
+++ ES6/Allison_es6/objects_and_classes/number_object/main.js
@@ -0,0 +1,37 @@
+"use strict";
+
+// What is a number object?
+// A Number object is a wrapper object that allows you to work with numerical values as objects. 
+// In JavaScript, primitive number values are not objects, but when you create a Number object using the Number constructor,
+// it wraps the primitive value in an object.
+
+// What are primitive number values?
+// Primitive number values are the basic data type for numbers in JavaScript. 
+// They are not objects and do not have methods or properties.
+// Examples of primitive number values include: 42, -3.14, 0, and NaN.
+
+// Create a Number object with the value 123
+// new => is used to create an instance of an object.
+let numObj = new Number(123);
+console.log(typeof numObj);
+
+var i = new Number(67.463098);
+console.log(i); // Output: [Number: 67.463098]
+
+// Demonstrating that numObj is an object
+console.log(numObj instanceof Number); // Output: true
+
+console.log("");
+console.log("Demo of Number object vs Primitive number value:");
+
+// Demonstrating that primitive number is not an object
+let primitiveNum = 123;
+console.log(typeof primitiveNum);
+console.log(primitiveNum instanceof Number); // Output: false
+
+console.log("");
+console.log("Using Number object methods:");
+
+// Using methods of Number object
+console.log(numObj.toFixed(2)); // Output: "123.00"
+console.log(numObj.valueOf());   // Output: 123
