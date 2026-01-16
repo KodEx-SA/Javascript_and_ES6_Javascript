009d085b4507b378f2dc1d9a0de0e9c3e0004cdc
Ashley K Motsie <motsieashley31@gmail.com>
2026-01-16 07:06:03 +0200

    date obj

---
+++ ES6/Allison_es6/objects_and_classes/date_object/main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// Date Object
+var dateObject = new Date();
+console.log(dateObject);
+
+// Create a new Date object representing the current date and time
+const currentDate = new Date();
+console.log("Current Date and Time:", currentDate);
+
+// Create a Date object for a specific date and time
+const specificDate = new Date("2023-01-01T12:00:00");
+console.log("Specific Date and Time:", specificDate);
