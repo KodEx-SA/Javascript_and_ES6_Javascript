77ee82fb616df889fbe3ed829260240e90d096dc
Ashley K Motsie <motsieashley31@gmail.com>
2026-01-16 07:54:03 +0200

    date object and methods

--- ES6/Allison_es6/objects_and_classes/date_object/main.js
+++ ES6/Allison_es6/objects_and_classes/date_object/main.js
@@ -3,6 +3,30 @@
 // Date Object
 var dateObject = new Date();
 console.log(dateObject);
+console.log(`Year: ${dateObject.getFullYear()}`);
+console.log(`Month: ${dateObject.getMonth() + 1}`); // Months are zero-indexed
+console.log(`Day of the Week: ${dateObject.getDay()}`); // 0 (Sunday) to 6 (Saturday)
+console.log(`Date: ${dateObject.getDate()}`);
+console.log(`Hours: ${dateObject.getHours()}`);
+console.log(`Minutes: ${dateObject.getMinutes()}`);
+console.log(`Seconds: ${dateObject.getSeconds()}`);
+console.log(`Milliseconds: ${dateObject.getMilliseconds()}`);
+console.log(`Time (ms since Jan 1, 1970): ${dateObject.getTime()}`);
+
+console.log("");
+
+console.log(`tostring: ${dateObject.toString()}`); // Full date and time
+console.log(`toDateString: ${dateObject.toDateString()}`); // Date portion
+console.log(`toTimeString: ${dateObject.toTimeString()}`); // Time portion
+
+console.log("");
+console.log(`toISOString: ${dateObject.toISOString()}`); // ISO 8601 format => YYYY-MM-DDTHH:mm:ss.sssZ => Coordinated Universal Time (UTC)
+// YYYY-MM-DDTHH:mm:ss.sssZ -> Year-Month-Day T Hours:Minutes:Seconds.Milliseconds Z (indicates UTC)
+
+console.log(`toUTCString: ${dateObject.toUTCString()}`); // UTC format => Day, DD Mon YYYY HH:mm:ss GMT
+// Day, DD Mon YYYY HH:mm:ss GMT -> Day of week, Day Month Year Hours:Minutes:Seconds GMT
+
+console.log("");
 
 // Create a new Date object representing the current date and time
 const currentDate = new Date();
