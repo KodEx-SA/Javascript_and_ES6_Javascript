91709f202709c1fe65c844531f01dd007b5bdf76
Ashley K Motsie <motsieashley31@gmail.com>
2026-01-16 06:24:25 +0200

    primitive bool value

--- ES6/Allison_es6/objects_and_classes/boolean_object/main.js
+++ ES6/Allison_es6/objects_and_classes/boolean_object/main.js
@@ -1,7 +1,18 @@
 "use strict";
 
 let boolObj = new Boolean(false); // Creates a Boolean object with the value false
+console.log(boolObj); // Outputs: [Boolean: false]
+
+// valueOf() method returns the primitive boolean value
+console.log(boolObj.valueOf()); // Outputs: false
+// typeof operator shows that boolObj is an object
+console.log(typeof boolObj); // Outputs: object
 
 if (boolObj) {
     console.log("This line is printed because boolObj is an object, which is truthy.");
 }
+
+console.log("");
+
+let primBool = false; // Primitive boolean value
+console.log(primBool); // Outputs: false
